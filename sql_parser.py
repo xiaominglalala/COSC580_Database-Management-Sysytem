8bb2837f545823b10aa799aef4ad12d51b1bc0a6
biubiupiu000 <64238764+biubiupiu000@users.noreply.github.com>
2022-04-20 21:06:58 -0400

    Update sql_parser.py

--- sql_parser.py
+++ sql_parser.py
@@ -277,3 +277,102 @@ def parse_update(sql):
 # parse_three_part("INSERT INTO play (index,name) VALUES (4,rob);")
 # parse_three_part("UPDATE play SET name=gggg WHERE id=c;")
 # parse_three_part("DELETE FROM play WHERE id=c;")
+
+def parse_select(sql):
+    keywords=['select','from','inner join','outer join','left join','right join','where','order by','group by']
+    sql=sql.lower()
+    select_lists = sqlparse.format(sql, reindent=True, indent_tabs=True, comma_first=True).split('\n')
+
+    select_lists = [i.strip() for i in select_lists]
+    join_part=None
+    where_part=None
+    for i, p in enumerate(select_lists):
+        if 'where' in p:
+            where_part = select_lists[i:]
+        elif 'from' in p:
+            from_part = p
+        elif 'order by' in p:
+            order_part=select_lists[i:]
+        elif 'join' in p:
+            join_part=p
+
+    # parse columns from select
+    select_columns = re.findall(r'select (.+) from', sql)
+    if len(select_columns)==0:
+        print("The sql syntax is wrong!")
+        return None
+    columns=select_columns[0].split(',')
+
+    # parse tables from select
+
+    res=from_part.split()[1:]
+    if len(res)>1:
+        tables = res
+        alias = res[1]
+    else:
+        tables=res
+    if join_part:
+        res=join_part.split()
+        join_type = res[0]
+        join_table = res[2]
+        join_key = tuple(res[-1].split('='))
+        tables.append((join_type,join_table,join_key))
+
+    # parse conditions
+    conditions = []
+    if where_part:
+        where_part=' '.join([i for i in where_part if i.split()[0]=='where'or i.split()[0]=='and' or i.split()[0]=='or'])[6:].split()
+        for i in where_part:
+            if i == 'and' or i == 'or':
+                conditions.append(i)
+            else:
+                if "=" in i:
+                    if "!=" in i:
+                        index = i.find('!=')
+                        c, v = i[:index], i[index + 2:]
+                        conditions.append((c, '!=', v))
+                    elif ">=" in i:
+                        index = i.find('>=')
+                        c, v = i[:index], i[index + 2:]
+                        conditions.append((c, '>=', v))
+                    elif "<=" in i:
+                        index = i.find('<=')
+                        c, v = i[:index], i[index + 2:]
+                        conditions.append((c, '<=', v))
+                    else:
+                        index = i.find('=')
+                        c, v = i[:index], i[index + 1:]
+                        conditions.append((c, '=', v))
+
+                elif "<" in i:
+                    index = i.find('<')
+                    c, v = i[:index], i[index + 1:]
+                    conditions.append((c, '<', v))
+                elif ">" in i:
+                    index = i.find('>')
+                    c, v = i[:index], i[index + 1:]
+                    conditions.append((c, '>', v))
+                else:
+                    print('Wrong syntax for conditions')
+                    return None
+
+    # parse orderby
+
+    select_columns = re.findall(r'order by (.+)', sql)
+    order_columns=[]
+    if len(select_columns)!=0:
+        select_columns= select_columns[0].split(',')
+        for i in select_columns:
+            if len(i.split())==2:
+                order_columns.append((i.split()[0],i.split()[1]))
+            else:
+                order_columns.append((i.split()[0],'aesc'))
+            if i.split()[0] not in columns:
+                if columns==['*']:
+                    pass
+                else:
+                    print('Order by column is not in the select columns!')
+                    return None
+    else:
+        order_columns=select_columns
+    return [columns,tables,conditions,order_columns]
